b67e2a2fc8cd524ae93294680265e89c6c78be13
Damon Burda <damonburda96@gmail.com>
2021-02-21 15:44:56 -0800

    all tests pass

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,15 +1,15 @@
 const Intern = require('../lib/Intern');
 
 test("Should assign school", () => {
-    const testValue = "UCI";
-    const e = new Intern("Damon", 8888, "damonburda96@gmail.com", testValue);
-    expect(e.school).toBe(testValue);
+    const uni = "UCI";
+    const e = new Intern("Damon", 8888, "damonburda96@gmail.com", uni);
+    expect(e.school).toBe(uni);
 });
 
 test("Should retrieve school from getSchool()", () => {
-    const testValue = "UCI";
-    const e = new Intern("Damon", 8888, "damonburda96@gmail.com", testValue);
-    expect(e.getSchool()).toBe(testValue);
+    const uni = "UCI";
+    const e = new Intern("Damon", 8888, "damonburda96@gmail.com", uni);
+    expect(e.getSchool()).toBe(uni);
 });
 
 test("Should retrieve role from getRole()", () => {
